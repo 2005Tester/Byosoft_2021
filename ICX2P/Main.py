e373b412253a4bdf0614e14bc47f09158ec61ae6
Huang Kai <huangkai@byosoft.com.cn>
2021-05-14 14:19:48 +0800

    rw py name rule

--- ICX2P/Main.py
+++ ICX2P/Main.py
@@ -1,7 +1,8 @@
 from Common import Unitool
 from Core import SutInit
 from Core.SutInit import Sut
-from ICX2P import UpdateBIOS, biosTest, DefaultValueTest, Os, Release, Smbios, Pwd, Legacy, DIMM, Cpu, Pch, Hotkey, PCIe
+from ICX2P import UpdateBIOS, biosTest, DefaultValueTest, Os, Release, Legacy, CpuInit01, MemInit02, PchInit03, \
+    PcieInit04, Io05, Smbios09, Security22
 from ICX2P.Config import SutConfig
 
 
@@ -19,42 +20,42 @@ def TestScope():
     biosTest.post_test(ser)
     biosTest.PM()
     biosTest.usbTest()
-    Cpu.cpu_mem_info()
+    CpuInit01.cpu_mem_info()
     biosTest.press_f2()
-    Cpu.static_turbo_default()
-    Cpu.ufs_default_value()
+    CpuInit01.static_turbo_default()
+    CpuInit01.ufs_default_value()
     DefaultValueTest.rrqirq()
     biosTest.dram_rapl_option_check()
     biosTest.securityBoot()
     biosTest.vtd()
     biosTest.cnd_default_enable()
-    Cpu.upi_link_status()
-    Cpu.cpu_cores_active_enable_1(ssh_os)
-    Cpu.cpu_cores_active_enable_middle(ssh_os)
-    Cpu.cpu_cores_active_enable_max(ssh_os)
-    Cpu.cpu_cores_disable_sys_normally(ssh_os)
-    Cpu.cores_customized_by_unitool(ssh_os)
+    CpuInit01.upi_link_status()
+    CpuInit01.cpu_cores_active_enable_1(ssh_os)
+    CpuInit01.cpu_cores_active_enable_middle(ssh_os)
+    CpuInit01.cpu_cores_active_enable_max(ssh_os)
+    CpuInit01.cpu_cores_disable_sys_normally(ssh_os)
+    CpuInit01.cores_customized_by_unitool(ssh_os)
     if Os.boot_to_suse():
-        Smbios.smbios_test_all(ssh_os)
+        Smbios09.smbios_test_all(ssh_os)
         Release.equip_mode_flag_check(unitool)
-    Pwd.Pwd_test(ser)
-    DIMM.dimm_power_mgt_01()
-    DIMM.dimm_power_mgt_02()
-    DIMM.dimm_power_mgt_04()
-    DIMM.dimm_power_mgt_05()
-    DIMM.dimm_power_mgt_07()
+    Security22.Pwd_test(ser)
+    MemInit02.dimm_power_mgt_01()
+    MemInit02.dimm_power_mgt_02()
+    MemInit02.dimm_power_mgt_04()
+    MemInit02.dimm_power_mgt_05()
+    MemInit02.dimm_power_mgt_07()
     Release.me_version_status()
     biosTest.loadDefault()
-    DIMM.Testcase_MemMargin_001()
-    Pch.usb_default_enable_check()
-    Pch.post_gpio_error_check()
+    MemInit02.Testcase_MemMargin_001()
+    PchInit03.usb_default_enable_check()
+    PchInit03.post_gpio_error_check()
     DefaultValueTest.pcie_port_bandwidth_check()
     biosTest.Testcase_SerialPrint_001()
     biosTest.Testcase_SerialPrint_003()
-    PCIe.Testcase_PCIeResource_001()
-    PCIe.Testcase_PCIeResource_002()
-    PCIe.Testcase_PCIeResource_003()
-    PCIe.Testcase_PCIeResource_005()
+    PcieInit04.Testcase_PCIeResource_001()
+    PcieInit04.Testcase_PCIeResource_002()
+    PcieInit04.Testcase_PCIeResource_003()
+    PcieInit04.Testcase_PCIeResource_005()
     biosTest.Testcase_PowerEfficiency_001(unitool)
     if Legacy.enable_legacy_boot():
         Legacy.disable_legacy_boot()
@@ -64,8 +65,8 @@ def TestScope():
 def EquipScope():
     Release.equip_mode_version_check()
     Os.boot_to_suse_mfg()
-    Smbios.smbios_type128(unitool)
-    DIMM.Testcase_MemoryCompa_009(unitool)
+    Smbios09.smbios_type128(unitool)
+    MemInit02.Testcase_MemoryCompa_009(unitool)
 
 
 # Define test scope for daily test
@@ -85,10 +86,10 @@ def ReleaseTest():
 
 def Debug():
     UpdateBIOS.update_bios('master')
-    Cpu.cpu_mem_info()
-    DIMM.Testcase_MemoryCompa_001()
-    DIMM.Testcase_MemoryCompa_006(ser, ssh_os)
-    Hotkey.Testcase_SystemInfo_001(ser)
-    Hotkey.Testcase_SystemInfo_002(ser)
-    Hotkey.Testcase_SystemInfo_003(ser)
+    CpuInit01.cpu_mem_info()
+    MemInit02.Testcase_MemoryCompa_001()
+    MemInit02.Testcase_MemoryCompa_006(ser, ssh_os)
+    Io05.Testcase_SystemInfo_001(ser)
+    Io05.Testcase_SystemInfo_002(ser)
+    Io05.Testcase_SystemInfo_003(ser)
     biosTest.Testcase_PowerEfficiency_001(unitool)
