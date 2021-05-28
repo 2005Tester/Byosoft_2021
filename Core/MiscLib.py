598c8fade42e55db7e77111b3c10796776e66406
Barton <gaojie@byosoft.com.cn>
2021-05-28 11:12:02 +0800

    [Core]Add common function to verify a bunch of messages in a str

--- Core/MiscLib.py
+++ Core/MiscLib.py
@@ -1,6 +1,7 @@
 import logging
 import subprocess
 import time
+import re
 
 # function library to hold platform indepedent operations, to simplify test case developemnt.
 
@@ -21,3 +22,18 @@ def ping_sut(ip, timeout):
         if time_spent > timeout:
             logging.error("Lost SUT for %s seconds, check the network connection" % time_spent)
             return False
+
+
+# Verify a bunch of messages (list) in a captured log (str)
+def verify_msgs_in_log(msg_list, captured_log):
+    result = True
+    if not captured_log:
+        logging.error("Nothing in input log")
+        return
+    for msg in msg_list:
+        if re.search(msg, captured_log):
+            logging.info("Verified: {0}".format(msg))
+        else:
+            logging.info("Not verified: {0}".format(msg))
+            result = False
+    return result
