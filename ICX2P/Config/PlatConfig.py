cacf2c8202281a75aa5aff758f661805ad531c0c
Barton Gao <gaojie@byosoft.com.cn>
2021-06-15 10:59:42 +0800

    [ICX2P]Update BMC info to fix systeminfo002 and 003 test

--- ICX2P/Config/PlatConfig.py
+++ ICX2P/Config/PlatConfig.py
@@ -137,7 +137,7 @@ class Msg:
     RC_VERSION = '0.2.2.0030'
     BIOS_REVISION = '0.12'
     BIOS_DATE = '05/25/2021'
-    iBMC_VERSION = '3.01.15.03'
+    iBMC_VERSION = '3.01.15.06'
     iBMC_IP = '192.168.2.102'
     CPU_TYPE = 'Ice Lake'
     TOTAL_MEMORY = '65536MB'
