1724036f29d59f005cae59534b9adce0eb13ff3e
Fu Baolin <fubaolin@byosoft.com.cn>
2021-10-13 16:25:40 +0800

    [icx2p]add EquipmentModeFlag_test [TEC] add EquipmentModeFlag_test

--- ICX2P/TestCase/Os.py
+++ ICX2P/TestCase/Os.py
@@ -1,9 +1,9 @@
 import logging
-from Core import SerialLib
+from Core import SerialLib, SshLib, MiscLib
 from Core.SutInit import Sut
 from ICX2P.Config import SutConfig
-from ICX2P.Config.PlatConfig import Key, Msg
-from ICX2P.BaseLib import SetUpLib
+from ICX2P.Config.PlatConfig import Key, Msg, BiosCfg
+from ICX2P.BaseLib import SetUpLib, PlatMisc, BmcLib
 from Report import ReportGen
 
 # Test case ID: TC300-TC320
@@ -58,3 +58,32 @@ def move_suse_to_first():
         return
     result.log_pass()
     return True
+
+
+# Author: Fubaolin
+# 多次执行装备模式脚本测试
+# Precondition:EquipmentMode,linux-OS
+# OnStart: NA
+# OnComplete: NA
+def EquipmentModeFlag_test():
+    tc = ('303', '[TC303] Equipment_Mode_007', '多次执行装备模式脚本测试')
+    result = ReportGen.LogHeaderResult(tc)
+    n = 0
+    m = 0
+    assert BmcLib.force_reset()
+    try:
+        for n in range(5):
+            assert MiscLib.ping_sut(SutConfig.Env.OS_IP, 300)
+            for m in range(10):
+                res = Sut.UNITOOL.set_config(BiosCfg.EQUIP_FLAG)
+                if not res:
+                    result.log_fail()
+                    logging.info("**set equipment mode flag fail.")
+                    return
+            assert BmcLib.force_reset()
+        result.log_pass()
+        return True
+    except AssertionError:
+        result.log_fail()
+    finally:
+        BmcLib.clear_cmos()
